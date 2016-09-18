4c318f1148859d8f720bcb5b1bcae090041baf2e
catiecook <catiecook888@gmail.com>
2016-09-18 10:44:21 -0600

    added database to heroku

--- knexfile.js
+++ knexfile.js
@@ -7,22 +7,6 @@ module.exports = {
     connection: 'postgres://localhost/tried-that-blog'
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
